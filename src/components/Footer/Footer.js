1373ffd0f002dfa88ce1d516187bb7a2f6f46254
Luke Lancaster <luketlancaster@gmail.com>
2021-11-12 09:22:31 -0600

    Exercise 1: Fix color of footer links

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,15 +1,15 @@
-import React from 'react';
-
-import styles from './Footer.module.css';
+import React from "react";
+import styles from "./Footer.module.css";
 
 const Footer = () => {
   return (
     <footer className={styles.footer}>
-      All character assets taken from{' '}
+      All character assets taken from{" "}
       <a
         href="https://www.openpeeps.com/"
         target="_blank"
         rel="noopener noreferrer"
+        className={styles.link}
       >
         Open Peeps
       </a>
